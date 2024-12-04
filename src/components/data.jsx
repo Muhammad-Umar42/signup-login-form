67dcbb20de842ffc1201e8907900fd10c24a034b
umar <umrking77@gmail.com>
2024-12-04 12:52:42 +0500

    filter

---
+++ src/components/data.jsx
@@ -0,0 +1,63 @@
+const resturent = [
+  {
+    catagry: "pizza",
+    price: "1299",
+    image: "pizza.jpg",
+    name: "Super Supreme",
+  },
+  {
+    catagry: "pizza",
+    price: "1299",
+    image: "pizza.jpg",
+    name: "Super Supreme",
+  },
+  {
+    catagry: "sandwich",
+    price: "699",
+    image: "sandwich.jpg",
+    name: "Mexican Sandwich",
+  },
+  {
+    catagry: "pizza",
+    price: "1299",
+    image: "pizza.jpg",
+    name: "Super Supreme",
+  },
+  {
+    catagry: "burger",
+    price: "499",
+    image: "burger.jpg",
+    name: "Zingo Burger",
+  },
+  {
+    catagry: "pizza",
+    price: "1299",
+    image: "pizza.jpg",
+    name: "Super Supreme",
+  },
+  {
+    catagry: "pizza",
+    price: "1299",
+    image: "pizza.jpg",
+    name: "Super Supreme",
+  },
+  {
+    catagry: "sandwich",
+    price: "699",
+    image: "sandwich.jpg",
+    name: "Mexican Sandwich",
+  },
+  {
+    catagry: "pizza",
+    price: "1299",
+    image: "pizza.jpg",
+    name: "Super Supreme",
+  },
+  {
+    catagry: "burger",
+    price: "499",
+    image: "burger.jpg",
+    name: "Zingo Burger",
+  },
+];
+export default resturent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import resturent from "../components/data";
 
 const Home = () => {
   let navigate = useNavigate();
@@ -7,13 +8,55 @@ const Home = () => {
     localStorage.removeItem("user");
     navigate("/login");
   };
+  ///////////////FILTERDATA//////////
+  const [userInput, setUserInput] = useState("");
+
+  const result = resturent.filter((el) => {
+    return el.catagry?.includes(userInput);
+  });
+
+  if (result == "") {
+    confirm("tito patiya na laaa");
+  }
+
   return (
-    <div className="text-4xl">
-      <span className="text-white font-semibold">Welcome</span> Home Page
-      <button onClick={handelRemove} className="bg-red-400 p-3">
-        Logout
-      </button>
-    </div>
+    <>
+      <div className=" m-3 flex justify-around">
+        <button
+          onClick={handelRemove}
+          className="bg-red-500 rounded-lg font-bold p-3 text-4xl"
+        >
+          Logout
+        </button>
+        <div>
+          <input
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+            value={userInput}
+            className="p-3 text-2xl"
+            type="text"
+            placeholder="search Here"
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-8 max-w-7xl mx-auto">
+        {result.map((e, ind) => {
+          return (
+            <div
+              key={ind}
+              className="bg-black border-4 border-solid border-yellow-500 text-slate-200  flex p-8 w-full max-w-80"
+            >
+              <img className="w-32" src={e.image} alt="pizza" />
+              <div className="m-auto p-2">
+                <p className="font-semibold ">{e.name}</p>
+                <h2 className="text-yellow-500 font-bold">{e.price}</h2>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
